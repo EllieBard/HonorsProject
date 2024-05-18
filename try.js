3253fb34541cd187ee1f182b3d8f61371a67279f
EllieBard <138938677+EllieBard@users.noreply.github.com>
2024-05-18 12:46:51 -0400

    Update try.js

--- try.js
+++ try.js
@@ -1,5 +1,4 @@
 var inputBox = document.getElementById("numMiles");
-inputBox.style.display = "block";
 while (inputBox.value > 0){
   document.getElementById("errorMiles").style.display = "none";
 }
