529a9cef25a358f6a7721a2bd57229beb44d87de
EllieBard <138938677+EllieBard@users.noreply.github.com>
2024-05-18 13:19:17 -0400

    Update try.js

--- try.js
+++ try.js
@@ -3,9 +3,11 @@ var inputBox = document.getElementById("numMiles");
 function checkInput(){
   if (inputBox.value < 0){
      document.getElementById("errorMiles").style.display = "inline";
+      return false;
   }
   else{
      document.getElementById("errorMiles").style.display = "none";
+      return true;
   }
 }
 
