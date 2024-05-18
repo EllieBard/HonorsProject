e48292286cc48a70f339bb9020fea73f0d6daa0f
EllieBard <138938677+EllieBard@users.noreply.github.com>
2024-05-18 17:47:07 -0400

    Update try.js

--- try.js
+++ try.js
@@ -7,6 +7,7 @@ function checkInput(){
   }
   else{
     document.getElementById("numMiles").disabled = true;
+    document.getElementById("numMiles").text = inputBox.value;
      document.getElementById("errorMiles").style.display = "none";
     document.getElementById("milesSubmit").style.display = "none";
       return true;
