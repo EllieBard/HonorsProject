4ff9516bdbd9b0d864a089f3bc4ba56c969d15a7
EllieBard <138938677+EllieBard@users.noreply.github.com>
2024-05-18 11:42:34 -0400

    Update try.js

--- try.js
+++ try.js
@@ -1,2 +1,4 @@
-while (document.getElementById("numMiles").value > 0)
+document.getElementById("errorMiles").style.display = "initial";
+while (document.getElementById("numMiles").value > 0){
   document.getElementById("errorMiles").style.display = "none";
+}
