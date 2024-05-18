a9aaa06ccee970fb21f5b84852ce92ab8b5de437
EllieBard <138938677+EllieBard@users.noreply.github.com>
2024-05-18 11:43:00 -0400

    Update try.js

--- try.js
+++ try.js
@@ -1,4 +1,4 @@
-document.getElementById("errorMiles").style.display = "initial";
+document.getElementById("numMiles").style.display = "initial";
 while (document.getElementById("numMiles").value > 0){
   document.getElementById("errorMiles").style.display = "none";
 }
